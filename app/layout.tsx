300501869987455bc85e5f77928ba9cabe3368b4
Your Name <your-email@example.com>
2025-06-12 21:23:50 +0900

    Google AdSenseコードを<head>に追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja" className="h-full">
+      <head>
+        <Head>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7578897320709624" crossOrigin="anonymous"></script>
+        </Head>
+      </head>
       <body className={cn(inter.className, 'h-full bg-gray-50')}>
         <div className="min-h-full">
           {children}
